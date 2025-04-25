6eeda2c776026877334d96aaad69416479c2f6e2
Jose Merida <joseameridac@gmail.com>
2025-04-25 15:30:06 -0600

    feat: set up environment w/ docker

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hola")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable", dbUser, dbPassword, dbName, dbHost, dbPort)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error pinging database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Backend is running")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ db/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS series (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR UNIQUE NOT NULL,
+  ranking INTEGER NOT NULL,
+  status VARCHAR NOT NULL CHECK (status IN ('Watching', 'Plan to Watch', 'Dropped', 'Completed')),
+  current_episode INTEGER NOT NULL,
+  total_episodes INTEGER NOT NULL
+);
+
+
+INSERT INTO series (title, ranking, status, current_episode, total_episodes)
+VALUES 
+('Fullmetal Alchemist: Brotherhood', 10, 'Completed', 64, 64),
+('One Piece', 9, 'Watching', 1100, 1100),
+('Death Note', 8, 'Completed', 37, 37),
+('Vinland Saga', 8, 'Plan to Watch', 0, 48),
+('Bleach: Thousand-Year Blood War', 7, 'Watching', 26, 52);
+
+SELECT * FROM series;
